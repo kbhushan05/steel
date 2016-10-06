419e79d06106d1fcf4f1ca7293ffd012057295de
kbhushan05@gmail.com <kbhushan05@gmail.com>
2016-10-06 09:57:57 +0530

    adding api to filer based on supplier and status.

--- src/main/java/com/demo/steel/api/OrderController.java
+++ src/main/java/com/demo/steel/api/OrderController.java
@@ -1,6 +1,7 @@
 package com.demo.steel.api;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.steel.dto.SteelOrderDto;
+import com.demo.steel.util.StringUtil;
 
 @RestController
 @RequestMapping("/orders")
@@ -36,18 +38,46 @@ public class OrderController {
 		getService().updateOrder(orderDto);
 	}
 
-	@RequestMapping(method=RequestMethod.GET,path="/new",produces="application/json")
+	@RequestMapping(method=RequestMethod.GET,produces="application/json",path="/new")
 	@ResponseStatus(code = HttpStatus.OK)
 	public SteelOrderDto createNewOrder(@RequestParam String supplierName){
 		return getService().createNewOrder(supplierName);
 	}
 	
 	@RequestMapping(method=RequestMethod.GET,produces="application/json")
-	public List<SteelOrderDto> getAll(){
-		return getService().getAll();
+	public List<SteelOrderDto> getAll(@RequestParam(required = false) String supplierName, @RequestParam(required = false) String status){
+		List<SteelOrderDto> all = getService().getAll();
+		
+		if(!StringUtil.isEmpty(supplierName)&& ! StringUtil.isEmpty(status)){
+			List<SteelOrderDto> onlySupplier = filterBySupplierName(all, supplierName);
+			List<SteelOrderDto> supplierAndStatus = filterByStatus(onlySupplier, status);
+			return supplierAndStatus;
+		}
+		
+		if(!StringUtil.isEmpty(supplierName)){
+			return filterBySupplierName(all, supplierName);
+		}
+		
+		if(!StringUtil.isEmpty(status)){
+			return filterByStatus(all, status);
+		}
+		
+		return all;
 	}
 	
-	@RequestMapping(path="/{orderId}", method=RequestMethod.PUT)
+	private List<SteelOrderDto> filterBySupplierName(List<SteelOrderDto> list, String supplierName){
+		return list.stream()
+		.filter(dto -> dto.getSupplierName().equals(supplierName))
+		.collect(Collectors.toList());
+	}
+	
+	private List<SteelOrderDto> filterByStatus(List<SteelOrderDto> list, String status){
+		return list.stream()
+		.filter(dto -> dto.getStatus().equalsIgnoreCase(status))
+		.collect(Collectors.toList());
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,path="/{orderId}")
 	public SteelOrderDto executeAction(@PathVariable long orderId, @RequestParam(required =false) String action,@RequestBody SteelOrderDto orderDto){
 		
 		switch (action) {
@@ -67,14 +97,9 @@ public class OrderController {
 		return orderDto;
 	}
 	
-	@RequestMapping(path="/{orderId}", method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET,path="/{orderId}")
 	public SteelOrderDto get(@PathVariable long orderId){
 			return getService().getOrder(orderId);
-	}
-	
-	@RequestMapping
-	public List<SteelOrderDto> getOrdersFromStatus(@RequestParam String status){
-		return getService().getOrdersFromStatus(status);
 	}
 	
 	public RestApiService getService() {
